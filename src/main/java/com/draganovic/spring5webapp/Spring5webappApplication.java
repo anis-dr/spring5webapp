50442ef84fd0f27cddb312a80e3193a1a3295bc6
anis00723 <anis00723@gmail.com>
2019-10-15 02:26:14 +0100

    Init

--- src/main/java/com/draganovic/spring5webapp/Spring5webappApplication.java
+++ src/main/java/com/draganovic/spring5webapp/Spring5webappApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Spring5webappApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(Spring5webappApplication.class, args);
     }
